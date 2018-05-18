c58a0a6457b1fd5e8eb2c660c282e56ce1cc1202
Collier <cole.hansen@gmail.com>
2018-05-17 20:26:43 -0700

    Add working server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-var app = require("express");
+var express = require("express");
+var path = require("path");
 
-
+var app = express();
 var port = process.env.PORT || 3000;
 
 app.listen(port, function() {
